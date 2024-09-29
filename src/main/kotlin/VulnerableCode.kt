fe0da35c0920e12a78913e937b374a053a59232f
Aykhan064 <122143508+Aykhan064@users.noreply.github.com>
2024-09-29 21:20:08 +0400

    Update VulnerableCode.kt

--- src/main/kotlin/VulnerableCode.kt
+++ src/main/kotlin/VulnerableCode.kt
@@ -1,6 +1,6 @@
 class VulnerableCode {
-    fun insecureLogin(username: String, password: String): Boolean {
-        val storedPassword = "password123"
-        return password == storedPassword
+    fun insecureLogin(username: String, pass: String): Boolean {
+        val password = "password123"
+        return pass == password
     }
 }
